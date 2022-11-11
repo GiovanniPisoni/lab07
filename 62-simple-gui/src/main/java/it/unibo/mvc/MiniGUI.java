8f3c5addb6cf633ab29a44178a6e8ba4aa733057
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2022-11-11 22:54:51 +0100

    62 Finished and checked

--- 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,8 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -16,7 +19,7 @@ import java.util.Random;
  * such is just to be used to learn the basics, not as a template for your
  * applications.
  */
-public class MiniGUI {
+public class MiniGUI { //nome simpatico 
 
     private static final String TITLE = "A very simple GUI application";
     private static final int PROPORTION = 5;
@@ -29,8 +32,19 @@ public class MiniGUI {
     public MiniGUI() {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
+        /* 
+        part 1 
+        */
+        final JPanel newPanel = new JPanel();
+        newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.LINE_AXIS));
+        canvas.add(newPanel, BorderLayout.CENTER);
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
+        newPanel.add(write);
+        /* 
+        part 2 
+        */
+        final JTextField result = new JTextField();
+        canvas.add(result, BorderLayout.NORTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -39,7 +53,12 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                /* 
+                part 3 
+                */
+                final int outNumber = randomGenerator.nextInt();
+                System.out.println(outNumber);
+                result.setText( Integer.toString(outNumber));
             }
         });
     }
